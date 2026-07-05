93631a0221dba3a2f2fee51946b9162e653221d7
Petar Kazandzhiev <petar.kazandzhiev@gmail.com>
2026-07-05 11:33:12 +0300

    Add initial HTML structure, CSS styles, and JavaScript functionality; implement button to update player name and basic math exercises

---
+++ Odin-js/index-math-exercise2.js
@@ -0,0 +1,15 @@
+// const myInt = 5;
+// const myFloat = 6.667;
+// console.log("myInt is a " + typeof myInt);
+// console.log("myFloat is a " + typeof myFloat);
+
+// const lotsOfDecimal = 1.7665849587;
+// console.log(lotsOfDecimal);
+// const twoDecimalPlaces = lotsOfDecimal.toFixed(4);
+// console.log(twoDecimalPlaces);
+
+
+let myNumber = "74";
+myNumber = Number(myNumber) + 3;
+console.log(myNumber);
+console.log(typeof myNumber);
---
+++ Odin-js/index-math-exercises.js
@@ -0,0 +1,15 @@
+let a = 10
+
+let maxRange = 7 * a
+let max_range = 7 * a
+
+
+console.log(maxRange)
+
+const MAX_RANGE = 57
+
+const actual = MAX_RANGE - 13
+
+const percentage = actual / MAX_RANGE
+
+console.log(percentage)
