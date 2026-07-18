72a5fc4c2eacff955184643004fb1907d6b6d4bf
Petar Kazandzhiev <petar.kazandzhiev@gmail.com>
2026-07-18 11:47:14 +0300

    Added js exercises folder

---
+++ javascript-exercises/helloWorld.js
@@ -0,0 +1,5 @@
+const helloWorld = function() {
+  return 'chao';
+}
+
+module.exports = helloWorld;
---
+++ javascript-exercises/joinStrings-example.js
@@ -0,0 +1,20 @@
+const birthYear = 1947;
+const thisYear = 1965;
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
+
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
+
+console.log(greeting);
+
+module.exports = {
+  greeting,
+  birthYear,
+  thisYear,
+  firstName,
+  lastName,
+  age
+
+}
