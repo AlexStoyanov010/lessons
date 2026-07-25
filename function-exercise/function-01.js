46552f1cafc722c304ff4317095043c3c47e423b
Petar Kazandzhiev <petar.kazandzhiev@gmail.com>
2026-07-25 13:31:51 +0300

    Add function definitions for add7, multiply, and capitalize; include example calls

---
+++ function-exercise/function-01.js
@@ -0,0 +1,22 @@
+const add7 = function(num) {
+  return num + 7;
+}
+
+
+add7(3); 
+
+
+const multiply = function(num1, num2) {
+  return num1 * num2;
+}
+
+multiply(3, 4);
+
+'example'
+
+'E' + 'xample'
+const capitalize = function(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+capitalize("hello");
